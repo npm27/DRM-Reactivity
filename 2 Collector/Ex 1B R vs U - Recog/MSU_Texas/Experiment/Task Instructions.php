6cb6f6370bb8f6dcbbc422ba2e902cfcb7f93619
Nicholas Maxwell <35810320+npm27@users.noreply.github.com>
2023-08-21 16:02:47 -0500

    Tweak task instructions

--- 2 Collector/Ex 1B R vs U - Recog/MSU_Texas/Experiment/Task Instructions.php
+++ 2 Collector/Ex 1B R vs U - Recog/MSU_Texas/Experiment/Task Instructions.php
@@ -1,9 +1,9 @@
 <section class="instructions">
     <h2 class="textcenter">Task Instructions</h2>
     <!-- ## SET ## Change the instructions text to match your task.-->
-    <p> In this study, you will be presented with a series of object words. </p>
-    <p> You will be asked to list as many possible uses for each object as you can think of. </p>
-    <p> It is important that you pay close attention to each object! Please close out of any other open windows/browser tabs! </p>
+    <p> In this study, you will be presented with a list of word</p>
+    <p> You will be asked to study each word. Please pay attention -- your memory will be tested!</p>
+    <p> Please close out of any other open windows/browser tabs! </p>
     <p> BEFORE STARTING: MAKE SURE TO NOTE WHAT TIME IT IS RIGHT NOW! You will be asked to provide this information at the end of the study! </p>
     <p> Please click advance when you are ready to continue. </p>
 
