35bc70fa01cb12f30d332a3b0419b9d6eba788f7
Nicholas Maxwell <35810320+npm27@users.noreply.github.com>
2024-10-02 15:08:23 -0500

    Submit to Memory

--- 4 Analysis/RTs/RT analyses.R
+++ 4 Analysis/RTs/RT analyses.R
@@ -38,3 +38,72 @@ read = subset(combined, combined$Encoding == "read")
 t.test(read$RT, global$RT, paired = F, var.equal = T)
 t.test(item$RT, global$RT, paired = F, var.equal = T)
 t.test(item$RT, read$RT, paired = F, var.equal = T)
+
+####Get individual experiments####
+##subset
+dat1.long = subset(combined, combined$experiment == "1A")
+dat2.long = subset(combined, combined$experiment == "1B")
+dat3.long = subset(combined, combined$experiment == "2")
+
+##means
+tapply(dat1.long$RT, list(dat1.long$Encoding, dat1.long$Direction), mean)
+tapply(dat2.long$RT, list(dat2.long$Encoding, dat2.long$Direction), mean)
+tapply(dat3.long$RT, list(dat3.long$Encoding, dat3.long$Direction), mean)
+
+##get CIs
+#Ex 1A
+d1.I = cast(subset(dat1.long,
+            dat1.long$Encoding == "item"),
+            Username ~ Direction, mean, value = "RT")
+
+(apply(d1.I, 2, sd) / sqrt(nrow(d1.I))) * 1.96
+
+d1.G = cast(subset(dat1.long,
+                   dat1.long$Encoding == "global"),
+            Username ~ Direction, mean, value = "RT")
+
+(apply(d1.G, 2, sd) / sqrt(nrow(d1.G))) * 1.96
+
+d1.R = cast(subset(dat1.long,
+                   dat1.long$Encoding == "read"),
+            Username ~ Direction, mean, value = "RT")
+
+(apply(d1.R, 2, sd) / sqrt(nrow(d1.R))) * 1.96
+
+#Ex 1B
+d2.I = cast(subset(dat2.long,
+                   dat2.long$Encoding == "item"),
+            Username ~ Direction, mean, value = "RT")
+
+(apply(d2.I, 2, sd) / sqrt(nrow(d2.I))) * 1.96
+
+d2.G = cast(subset(dat2.long,
+                   dat2.long$Encoding == "global"),
+            Username ~ Direction, mean, value = "RT")
+
+(apply(d2.G, 2, sd, na.rm = T) / sqrt(nrow(d2.G))) * 1.96
+
+d2.R = cast(subset(dat2.long,
+                   dat2.long$Encoding == "read"),
+            Username ~ Direction, mean, value = "RT")
+
+(apply(d2.R, 2, sd, na.rm = T) / sqrt(nrow(d2.R))) * 1.96
+
+#Ex 2
+d3.I = cast(subset(dat3.long,
+                   dat3.long$Encoding == "item"),
+            Username ~ Direction, mean, value = "RT")
+
+(sd(d3.I$V1) / sqrt(nrow(d3.I))) * 1.96
+
+d3.G = cast(subset(dat3.long,
+                   dat3.long$Encoding == "global"),
+            Username ~ Direction, mean, value = "RT")
+
+(sd(d3.G$V1) / sqrt(nrow(d3.I))) * 1.96
+
+d3.R = cast(subset(dat3.long,
+                   dat3.long$Encoding == "read"),
+            Username ~ Direction, mean, value = "RT")
+
+(sd(d3.R$V1) / sqrt(nrow(d3.I))) * 1.96
